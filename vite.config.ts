557d6a99af1e5382cb173c266538460c37361d76
p_yaossong <p_yaossong@tencent.com>
2023-09-24 01:06:00 +0800

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/nanjiu-tools/",
   plugins: [
     vue(),
     vueJsx(),
