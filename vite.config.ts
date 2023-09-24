b3acbab7602780ee9896572d27fbc48be0f2d966
p_yaossong <p_yaossong@tencent.com>
2023-09-24 12:52:46 +0800

    feat: 修改打包

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,37 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
 
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/nanjiu-tools/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    chunkSizeWarningLimit: 2000, // 解决包大小超过500kb的警告
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // elementPlus: ["element-plus"],
+          // highlightjs: ["highlight.js"],
+        },
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+        sanitizeFileName: (name) => {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "") // 处理文件名中的非法字符
+          );
+        },
+      },
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
