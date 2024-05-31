9ae9970921ca4ed59b277af070a56b26d06490d4
bettersong <39439471+bettersong@users.noreply.github.com>
2024-05-31 07:45:25 +0000

    Deploying to static-pages from @ bettersong/nanjiu-tools@ef313bd5e43a221f44fc4b9a8bf702614ece5000 🚀

---
+++ js/AboutView-232434b5.js
@@ -0,0 +1,1 @@
+import{d as e,h as t,a as o,o as _}from"./vendor-92f39675.js";const n={class:"about"},r=e({__name:"AboutView",setup(s){return t(()=>{}),(a,c)=>(_(),o("div",n))}});export{r as default};
---
+++ js/index-bded0129.js
@@ -0,0 +1,1 @@
+import{d as t,r as l,g as n,h as r,a as u,i as o,t as s,u as c,o as i}from"./vendor-92f39675.js";const _=t({__name:"index",setup(m){const e=l("nanjiu");let a=n({value:18});return n({name:"nanjiu",age:18}),r(()=>{console.log("name",e),console.log("age",a)}),(d,g)=>(i(),u("div",null,[o("div",null,"name: "+s(e.value),1),o("div",null,"age: "+s(c(a)),1)]))}});export{_ as default};
